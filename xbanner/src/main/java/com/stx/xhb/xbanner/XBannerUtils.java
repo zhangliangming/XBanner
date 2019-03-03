323322aa5cdd520be5011a79d32bd7265f57fa2c
zhangliangming <316257874@qq.com>
2019-03-03 21:39:16 +0800

    初始导入

---
+++ xbanner/src/main/java/com/stx/xhb/xbanner/XBannerUtils.java
@@ -0,0 +1,59 @@
+package com.stx.xhb.xbanner;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+/**
+ * <p>
+ * link https://xiaohaibin.github.io/
+ * email： xhb_199409@163.com
+ * github: https://github.com/xiaohaibin
+ * description： XBanner轮播控件的工具类
+ */
+public class XBannerUtils {
+
+    /**
+     * 设置背景选择器
+     *
+     * @param pressedDraw
+     * @param normalDraw
+     * @return
+     */
+    public static StateListDrawable getSelector(Drawable normalDraw, Drawable pressedDraw) {
+        StateListDrawable stateListDrawable = new StateListDrawable();
+        stateListDrawable.addState(new int[]{android.R.attr.state_enabled}, pressedDraw);
+        stateListDrawable.addState(new int[]{}, normalDraw);
+        return stateListDrawable;
+    }
+
+    public static Drawable getDrawable(Context context, @DrawableRes int resId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return context.getDrawable(resId);
+        } else {
+            return context.getResources().getDrawable(resId);
+        }
+    }
+
+    public static int dp2px(Context context, float dpValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, context.getResources().getDisplayMetrics());
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spValue, context.getResources().getDisplayMetrics());
+    }
+
+    public static int getScreenWidth(Context context) {
+        Resources resources = context.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        int width = dm.widthPixels;
+        return width;
+    }
+
+
+}
